efe629c04db2ff28d45018f49302e279813d8b3f
s1l3nt78 <44367179+s1l3n7h0s7@users.noreply.github.com>
2020-02-16 01:07:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -141,6 +141,7 @@ else
 	git clone https://github.com/threatexpress/domainhunter
 	cd domainhunter
 	sudo python3 -m pip install -r requirements.txt
+	sudo apt-get install python3-pil
 	cd ..
 fi
 
