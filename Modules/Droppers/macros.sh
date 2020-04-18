2621a63ddca6e2160c22beea4f38b0916b4bed5e
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-04-18 17:16:21 +0200

    macros

---
+++ Modules/Droppers/macros.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ORNG='\033[0;33m'
+NC='\033[0m'
+W='\033[1;37m'
+LP='\033[1;35m'
+YLW='\033[1;33m'
+LBBLUE='\e[104m'
+
+echo -e "${RED}"
+figlet Macros
+echo -e "${NC}"
+cd tools/macro_pack
