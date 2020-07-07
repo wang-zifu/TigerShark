eb6492b15fb17ba6d4a17dd43bcc718c73c29642
s1l3nt78 <44367179+s1l3nt78@users.noreply.github.com>
2020-07-07 22:43:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ LP='\033[1;35m'
 YLW='\033[1;33m'
 LBBLUE='\e[104m'
 RED='\033[0;31m'
-sudo apt-get install golang-1.11 golang-1.11-go golang-1.11-src git python2 python3 python3-pip python3-dev python3 virtualenv gnupg certbot wine wine64
+sudo apt-get install golang-1.* golang golang-1.*-go golang-1.*-src git python2 python3 python3-pip python3-dev python3 virtualenv gnupg certbot wine wine64
 echo -e "${YLW}This script will download and install all the tools needed for TigerShark to run."
 echo -e "If this is okay, hit ${LP}enter${YLW} - otherwise ${RED}Ctrl${W} + ${RED}c ${YLW}to cancel${NC}"
 read ACCEPT
